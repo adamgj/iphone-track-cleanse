4fb1e573e817e13aa6d66008364d879fccc594ad
AdamGJ <adam@modsrus.com>
2017-03-04 17:44:38 -0500

    initial code

---
+++ iPhoneTrackingCleanse/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace iPhoneTrackingCleanse
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbRoot = new System.Windows.Forms.ComboBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.dlgFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.pbScan = new System.Windows.Forms.ProgressBar();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnCleanse = new System.Windows.Forms.Button();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuBrowse = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuScan = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCleanse = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCancelScan = new System.Windows.Forms.Button();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbRoot
+            // 
+            this.cmbRoot.FormattingEnabled = true;
+            this.cmbRoot.Location = new System.Drawing.Point(78, 36);
+            this.cmbRoot.Name = "cmbRoot";
+            this.cmbRoot.Size = new System.Drawing.Size(353, 21);
+            this.cmbRoot.TabIndex = 1;
+            this.cmbRoot.Text = "D:\\iPhoneTrackingCleanseTestDir";
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(437, 34);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(84, 23);
+            this.btnScan.TabIndex = 0;
+            this.btnScan.Text = "&Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(11, 36);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(61, 21);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "&Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // dlgFolder
+            // 
+            this.dlgFolder.Description = "Select Apple Directory";
+            this.dlgFolder.RootFolder = System.Environment.SpecialFolder.ApplicationData;
+            this.dlgFolder.ShowNewFolderButton = false;
+            // 
+            // pbScan
+            // 
+            this.pbScan.Location = new System.Drawing.Point(10, 63);
+            this.pbScan.Name = "pbScan";
+            this.pbScan.Size = new System.Drawing.Size(511, 16);
+            this.pbScan.TabIndex = 5;
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.ForeColor = System.Drawing.Color.Red;
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(10, 85);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstFiles.Size = new System.Drawing.Size(511, 95);
+            this.lstFiles.TabIndex = 3;
+            // 
+            // btnCleanse
+            // 
+            this.btnCleanse.Enabled = false;
+            this.btnCleanse.Location = new System.Drawing.Point(10, 186);
+            this.btnCleanse.Name = "btnCleanse";
+            this.btnCleanse.Size = new System.Drawing.Size(110, 43);
+            this.btnCleanse.TabIndex = 4;
+            this.btnCleanse.Text = "&Cleanse";
+            this.btnCleanse.UseVisualStyleBackColor = true;
+            this.btnCleanse.Click += new System.EventHandler(this.btnCleanse_Click);
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuHelp});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(535, 24);
+            this.mnuMain.TabIndex = 6;
+            this.mnuMain.Text = "MainMenu";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuBrowse,
+            this.mnuScan,
+            this.mnuCleanse,
+            this.toolStripSeparator1,
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(35, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuBrowse
+            // 
+            this.mnuBrowse.Name = "mnuBrowse";
+            this.mnuBrowse.Size = new System.Drawing.Size(112, 22);
+            this.mnuBrowse.Text = "&Browse";
+            this.mnuBrowse.Click += new System.EventHandler(this.mnuBrowse_Click);
+            // 
+            // mnuScan
+            // 
+            this.mnuScan.Name = "mnuScan";
+            this.mnuScan.Size = new System.Drawing.Size(112, 22);
+            this.mnuScan.Text = "&Scan";
+            this.mnuScan.Click += new System.EventHandler(this.mnuScan_Click);
+            // 
+            // mnuCleanse
+            // 
+            this.mnuCleanse.Name = "mnuCleanse";
+            this.mnuCleanse.Size = new System.Drawing.Size(112, 22);
+            this.mnuCleanse.Text = "&Cleanse";
+            this.mnuCleanse.Click += new System.EventHandler(this.mnuCleanse_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(109, 6);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(112, 22);
+            this.mnuExit.Text = "E&xit";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // mnuHelp
+            // 
+            this.mnuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAbout});
+            this.mnuHelp.Name = "mnuHelp";
+            this.mnuHelp.Size = new System.Drawing.Size(40, 20);
+            this.mnuHelp.Text = "&Help";
+            // 
+            // mnuAbout
+            // 
+            this.mnuAbout.Name = "mnuAbout";
+            this.mnuAbout.Size = new System.Drawing.Size(103, 22);
+            this.mnuAbout.Text = "&About";
+            this.mnuAbout.Click += new System.EventHandler(this.mnuAbout_Click);
+            // 
+            // btnCancelScan
+            // 
+            this.btnCancelScan.Enabled = false;
+            this.btnCancelScan.Location = new System.Drawing.Point(437, 35);
+            this.btnCancelScan.Name = "btnCancelScan";
+            this.btnCancelScan.Size = new System.Drawing.Size(84, 22);
+            this.btnCancelScan.TabIndex = 1;
+            this.btnCancelScan.Text = "Cancel";
+            this.btnCancelScan.UseVisualStyleBackColor = true;
+            this.btnCancelScan.Visible = false;
+            this.btnCancelScan.Click += new System.EventHandler(this.btnCancelScan_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 239);
+            this.Controls.Add(this.btnCancelScan);
+            this.Controls.Add(this.btnCleanse);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.pbScan);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.cmbRoot);
+            this.Controls.Add(this.mnuMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mnuMain;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "ZeroTracks BETA 1.1 (TechnicalSupport.ca) by ModsRUs.com";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbRoot;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolder;
+        private System.Windows.Forms.ProgressBar pbScan;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnCleanse;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuBrowse;
+        private System.Windows.Forms.ToolStripMenuItem mnuCleanse;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.ToolStripMenuItem mnuHelp;
+        private System.Windows.Forms.ToolStripMenuItem mnuAbout;
+        private System.Windows.Forms.ToolStripMenuItem mnuScan;
+        private System.Windows.Forms.Button btnCancelScan;
+    }
+}
+
---
+++ iPhoneTrackingCleanse/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Reflection;
+
+
+namespace iPhoneTrackingCleanse
+{
+    public partial class frmMain : Form
+    {
+        private const string strVer = "BETA 1.1";
+        private string backupDir = Application.StartupPath + @"\Backup";
+        private string defaultDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+@"\Apple Computer\MobileSync\Backup";
+        private bool doBackup = false;
+        private const string strFSearch = "*.*";
+        private const string strHeader = "SQLite";
+        private const string strToFind = "CellLocation";
+        readonly object stateLock = new object();
+        private string strRoot;
+        private string curFName = "";
+        private List<string> lstFNames = new List<string>();
+        private List<string> lstTrackFiles = new List<string>();
+        private const string credit = "by Adam Jones";
+        private Thread workThread;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            BeginScan();
+        }
+
+        private void BeginScan()
+        {
+            if (Directory.Exists(cmbRoot.Text) == false)
+            {
+                MessageBox.Show("Error: Directory does not exist", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            DisableUI();
+            btnCancelScan.Enabled = true;
+            btnCancelScan.Visible = btnCancelScan.Enabled;
+            pbScan.Value = 0;
+            lstFiles.Items.Clear();
+            lstFNames.Clear();
+            lstFiles.ForeColor = Color.Red;
+            lstTrackFiles.Clear();
+            lock (stateLock)
+            {
+                strRoot = cmbRoot.Text;
+                curFName = "";
+            }
+            ThreadStart scanThreadStart = new ThreadStart(ScanJob);
+            lock (stateLock)
+            {
+                workThread = new Thread(scanThreadStart);
+            }
+            workThread.IsBackground = true;
+            workThread.Start();
+        }
+
+        private void DisableUI()
+        {
+            btnScan.Enabled = false;
+            btnBrowse.Enabled = false;
+            btnCleanse.Enabled = false;
+            mnuScan.Enabled = false;
+            mnuBrowse.Enabled = false;
+            mnuCleanse.Enabled = false;
+            mnuExit.Enabled = false;
+            cmbRoot.Enabled = false;
+        }
+
+        private void EnableUI()
+        {
+            btnScan.Enabled = true;
+            btnBrowse.Enabled = true;
+            mnuScan.Enabled = true;
+            mnuBrowse.Enabled = true;
+            mnuExit.Enabled = true;
+            cmbRoot.Enabled = true;
+            if (lstFiles.Items.Count > 0)
+            {
+                mnuCleanse.Enabled = true;
+                btnCleanse.Enabled = true;
+            }
+        }
+
+        private void CompleteScan()
+        {
+            pbScan.Value = pbScan.Maximum;
+            MessageBox.Show("Scan Complete!","Complete",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            btnCancelScan.Enabled = false;
+            btnCancelScan.Visible = btnCancelScan.Enabled;
+            EnableUI();
+
+        }
+
+        private void ScanJob()
+        {
+            MethodInvoker setupPB = new MethodInvoker(SetupPB);
+            string strLocalRoot;
+            lock (stateLock)
+            {
+                strLocalRoot = strRoot;
+            }
+            DirectoryInfo dirIRoot = new DirectoryInfo(strLocalRoot);
+            RecursiveDirScan(dirIRoot);
+            Invoke(setupPB);
+            ScanFiles();
+            Invoke(new MethodInvoker(CompleteScan));
+        }
+
+        private void SetupPB()
+        {
+            pbScan.Maximum = lstFNames.Count+1;
+            pbScan.Step = 1;
+            pbScan.PerformStep();
+        }
+
+        private void RecursiveDirScan(DirectoryInfo dirInfo)
+        {
+            if (dirInfo.Exists == false)
+                return;
+
+            DirectoryInfo[] curDirISubs = dirInfo.GetDirectories();
+            foreach (FileInfo fInfo in dirInfo.GetFiles(strFSearch))
+            {
+                lstFNames.Add(fInfo.FullName);
+            }
+            foreach (DirectoryInfo curDirI in curDirISubs)
+            {
+                RecursiveDirScan(curDirI);
+            }
+        }
+
+        private void ScanFiles()
+        {
+            MethodInvoker updateProgress = new MethodInvoker(UpdateProgress);
+            MethodInvoker addFile = new MethodInvoker(AddFile);
+            List<string> fNames;
+            lock (stateLock)
+            {
+                fNames = lstFNames;
+            }
+            foreach (string curLocalFName in fNames)
+            {
+                if (containsValidData(curLocalFName) == true)
+                {
+                    lock (stateLock)
+                    {
+                        curFName = curLocalFName;
+                    }
+                    Invoke(addFile);
+                }
+                Invoke(updateProgress);
+            }
+        }
+
+        private bool containsValidData(string curLocalFName)
+        {
+            if (File.Exists(curLocalFName) == false)
+                return false;
+
+            FileInfo curFInfo = new FileInfo(curLocalFName);
+            Stream fStream = null;
+            try
+            {
+                fStream = curFInfo.OpenRead();
+            }
+            catch
+            {
+                MessageBox.Show("Error: Could not read file: " + curLocalFName, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            BinaryReader bReader = new BinaryReader(fStream);
+
+            string localStrHeader;
+            lock (stateLock)
+            {
+                localStrHeader = strHeader;
+            }
+            char[] headerTest;
+            try
+            {
+                headerTest = bReader.ReadChars(localStrHeader.Length);
+            }
+            catch
+            {
+                return false;
+            }
+            if (String.Compare(new string(headerTest), localStrHeader) != 0)
+                return false;
+
+            int strLen;
+            lock (stateLock)
+            {
+                strLen = strToFind.Length;
+            }
+            long pos = 0;
+            byte[] previous = bReader.ReadBytes(strLen);
+
+            for (long i = bReader.BaseStream.Position; i < fStream.Length; i++)
+            {
+                byte current = bReader.ReadByte();
+                previous[pos++ % strLen] = current;
+
+                bool isValidStr = true;
+                for (int j = 0; j < strLen; j++)
+                {
+                    if (previous[(pos + j) % strLen] != strToFind[j])
+                    {
+                        isValidStr = false;
+                    }
+                }
+                if (isValidStr == true)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private void AddFile()
+        {
+            string curLocalFName;
+            lock (stateLock)
+            {
+                curLocalFName = curFName;
+            }
+            lstFiles.Items.Add(Path.GetFileName(curLocalFName));
+            lstTrackFiles.Add(curLocalFName);
+        }
+
+        private void UpdateProgress()
+        {
+            pbScan.PerformStep();
+        }
+
+        private void BrowseFolders()
+        {
+            if (dlgFolder.ShowDialog() == DialogResult.OK)
+                cmbRoot.Text = dlgFolder.SelectedPath;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            BrowseFolders();
+        }
+
+        private void btnCleanse_Click(object sender, EventArgs e)
+        {
+            BeginCleanse();
+        }
+
+        private void BeginCleanse()
+        {
+            DisableUI();
+            pbScan.Value = 0;
+            ThreadStart cleanseThreadStart = new ThreadStart(CleanseJob);
+            Thread cleanseThread = new Thread(cleanseThreadStart);
+            cleanseThread.IsBackground = true;
+            cleanseThread.Start();
+        }
+
+        private void CleanseJob()
+        {
+            MethodInvoker setupPB = new MethodInvoker(SetupPB);
+            MethodInvoker updateProgress = new MethodInvoker(UpdateProgress);
+            Invoke(setupPB);
+
+            foreach (string strLocalCurFile in lstTrackFiles)
+            {
+                try
+                {
+                    if (File.Exists(strLocalCurFile))
+                    {
+                        if (doBackup == true)
+                        {
+                            if (Directory.Exists(backupDir) == false)
+                                Directory.CreateDirectory(backupDir);
+                            File.Move(strLocalCurFile, backupDir + Path.GetFileName(strLocalCurFile));
+                        }
+                        else
+                            File.Delete(strLocalCurFile);
+                        File.Create(strLocalCurFile);
+                        /*Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("iPhoneTrackingCleanse.dummyConsolidated");
+                        FileStream fileStream = new FileStream(strLocalCurFile, FileMode.CreateNew);
+                        int iB =-1;
+                        do
+                        {
+                            iB = stream.ReadByte();
+                            fileStream.WriteByte((byte)iB);
+                        } while (iB != -1);
+                        fileStream.Close();*/
+
+                        Invoke(updateProgress);
+                    }
+                    else
+                        MessageBox.Show("Error: Could not find file " + strLocalCurFile, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch
+                {
+                    MessageBox.Show("Error: Could not access file " + strLocalCurFile, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            Invoke(new MethodInvoker(CompleteCleanse));
+        }
+
+        private void CompleteCleanse()
+        {
+            pbScan.Value = pbScan.Maximum;
+            lstFiles.ForeColor = Color.Green;
+            MessageBox.Show("Cleanse Complete!", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            EnableUI();
+            btnCleanse.Enabled = false;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            setAppleBackupDir();
+        }
+
+        private void setAppleBackupDir()
+        {
+            cmbRoot.Text = defaultDir;
+            cmbRoot.Items.Add(defaultDir);
+            if (Directory.Exists(defaultDir) == false)
+                MessageBox.Show("Error: Could not detect Apple Backup directory","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+        }
+
+        private void mnuBrowse_Click(object sender, EventArgs e)
+        {
+            BrowseFolders();
+        }
+
+        private void mnuCleanse_Click(object sender, EventArgs e)
+        {
+            BeginCleanse();
+        }
+
+        private void mnuScan_Click(object sender, EventArgs e)
+        {
+            BeginScan();
+        }
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mnuAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Application.ProductName+" "+strVer+"\n\tby ModsRUs.com\n\n\tTechnicalSupport.ca","About",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void StopWorkThread()
+        {
+            workThread.Abort();
+        }
+
+        private void btnCancelScan_Click(object sender, EventArgs e)
+        {
+            //workThread.Suspend();
+            if (MessageBox.Show("Are you sure you would like to cancel a scan in progress?", "Cancel scan?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
+            {
+                //workThread.Resume();
+                return;
+            }
+            StopWorkThread();
+            btnCancelScan.Enabled = false;
+            btnCancelScan.Visible = btnCancelScan.Enabled;
+            EnableUI();
+        }
+    }
+}
